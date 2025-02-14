13dafc380ca9e19a0b8488e50105917c9dd193b8
Uriel-CR <uriel202118529@ixtapaluca.tecnm.mx>
2025-02-14 13:51:44 -0600

    Update database connection credentials and database name in alumnos.php and conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conexion = mysqli_connect("localhost", "serviciosocial", "FtW30yNo8hQd-x/G","nuevo_sistema");
+$conexion = mysqli_connect("localhost", "root", "alociva07.PM","login-register-new");
 
 if (!$conexion) {
     error_log("Error de conexión a la base de datos: " . mysqli_connect_error());
